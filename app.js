134abcf33e318eeb5f37542794502e40146d70c4
Daniel Olivares <danieloddl2@hotmail.com>
2015-03-01 13:24:48 -0600

    Add server/restaurants/Lucky Wings/dishes.

--- app.js
+++ app.js
@@ -45,17 +45,9 @@ app.get("/restaurants/:restaurant_name", function(request,response){
 	response.send(result);
 });
 
-app.get("/:restaurant_name", function(request,response){
-	//Get a specific restaurant (i.e. localhost:3000/Lucky Wings)
-	var result = restaurants.filter(function( obj ) {
-		return obj.name == request.params.restaurant_name;
-	});
-	response.send(result);
-});
-
 app.get("/dishes", function(request,response){
 	//Get all dishes or dishes by restaurant (i.e. localhost:3000/dishes?restaurant_id=1)
-	if(!request.query.restaurant_id || request.query.restaurant_id <= 0){
+	if(!request.query.restaurant_id){
 		response.send(dishes);
 	}
 	else{
@@ -64,6 +56,20 @@ app.get("/dishes", function(request,response){
 		});
 		response.send(result);
 	}
+});
+
+app.get("/restaurants/:restaurant_name/dishes", function(request,response){
+	//Get all dishes or dishes by restaurant (i.e. localhost:3000/restaurant/Lucky Wings/dishes)
+
+	//Get restaurant ID from name
+	var restaurant = restaurants.filter(function( obj ) {
+		return obj.name == request.params.restaurant_name;
+	});
+
+	var result = dishes.filter(function( obj ) {
+		return obj.restaurant_id == restaurant[0].id;
+	});
+	response.send(result);
 });
 
 app.listen(3000, console.log("Listening on port 3000"));
