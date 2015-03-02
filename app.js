27b4c06104eb0edb88af19373c7fb18911da7729
Daniel Olivares <danieloddl2@hotmail.com>
2015-03-01 23:55:39 -0600

    Add Tables and simplify queries.

--- app.js
+++ app.js
@@ -31,45 +31,63 @@ var dishes = [
 	price: 60
 }
 ];
+var tables = [
+	{
+		restaurant_id: 1,
+		number: 1,
+		description: "Just around the corner"
+	},
+	{
+		restaurant_id: 1,
+		number: 2,
+		description: "Just beside the bathroom"
+	},
+	{
+		restaurant_id: 2,
+		number: 1,
+		description: "With movable chairs"
+	},
+	{
+		restaurant_id: 3,
+		number: 1,
+		description: "Test table"
+	},
+];
 
-app.get("/restaurants", function(request, response){
-	//Get all restaurants
+app.get("/", function(request, response){
+	response.send("Index");
+});
+
+app.get("/restaurants", function(request, response){ 	//Get all restaurants
 	response.send(restaurants);
 });
 
-app.get("/restaurants/:restaurant_name", function(request,response){
-	//Get a specific restaurant (i.e. localhost:3000/restaurants/Lucky Wings)
-	var result = restaurants.filter(function( obj ) {
-		return obj.name == request.params.restaurant_name;
-	});
-	response.send(result);
+app.get("/restaurants/:restaurant_name", function(request,response){	//Get a specific restaurant (i.e. localhost:3000/restaurants/Lucky Wings)
+	response.send(findBy(restaurants,"name", request.params.restaurant_name));
 });
 
-app.get("/dishes", function(request,response){
-	//Get all dishes or dishes by restaurant (i.e. localhost:3000/dishes?restaurant_id=1)
-	if(!request.query.restaurant_id){
-		response.send(dishes);
-	}
-	else{
-		var result = dishes.filter(function( obj ) {
-			return obj.restaurant_id == request.query.restaurant_id;
-		});
-		response.send(result);
-	}
-});
-
-app.get("/restaurants/:restaurant_name/dishes", function(request,response){
-	//Get all dishes or dishes by restaurant (i.e. localhost:3000/restaurant/Lucky Wings/dishes)
-
+app.get("/restaurants/:restaurant_name/dishes", function(request,response){		//Get all dishes or dishes by restaurant (i.e. localhost:3000/restaurant/Lucky Wings/dishes)
 	//Get restaurant ID from name
-	var restaurant = restaurants.filter(function( obj ) {
-		return obj.name == request.params.restaurant_name;
-	});
+	var restaurant = findBy(restaurants, "name", request.params.restaurant_name)[0].id;
 
-	var result = dishes.filter(function( obj ) {
-		return obj.restaurant_id == restaurant[0].id;
-	});
-	response.send(result);
+	response.send(findBy(dishes,"restaurant_id", restaurant));
 });
 
-app.listen(3000, console.log("Listening on port 3000"));
+app.get("/restaurants/:restaurant_name/tables/", function(request,response){	//Get all tables for a restaurant
+	//Get restaurant ID from name
+	var restaurant = findBy(restaurants, "name", request.params.restaurant_name)[0].id;
+
+	response.send(findBy(tables, "restaurant_id", restaurant));
+});
+
+function findBy(arrayOfObjects, property, value){
+	var finalArray = [];
+	var result = arrayOfObjects.filter(function(obj){
+		if(obj[property] == value){
+			finalArray.push(obj);
+		}
+	});
+		return finalArray;
+}
+
+app.listen(3000, console.log("Listening on port 3000")); 
